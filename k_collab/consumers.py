1463b99bfe7ec6e64d76af86e9499d3fa50b8d36
Abinash Kalita <118420585+itz-A53-K@users.noreply.github.com>
2025-04-09 02:28:51 +0530

    Update README and remove unused fields from User model

--- k_collab/consumers.py
+++ k_collab/consumers.py
@@ -1,9 +1,7 @@
-import json, requests
+import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
-from django.db.models import Q
-from asgiref.sync import async_to_sync
 from datetime import datetime
 from api import models, serializers
 
